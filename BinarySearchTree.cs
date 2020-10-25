3e152a5cdef855b8407e793bca46fad35158a2ac
AkshayPoriya <akshayporiya1234@gmail.com>
2020-10-26 02:32:43 +0530

    [Akshay] UC1 Create Simple BST

---
+++ BinarySearchTree.cs
@@ -0,0 +1,91 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BinarySearchTree.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akshay Poriya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BST_DS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// BST Class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class BinarySearchTree<T> where T:IComparable
+    {
+        /// <summary>
+        /// The root data
+        /// </summary>
+        public T rootData;
+
+        /// <summary>
+        /// The left branch tree
+        /// </summary>
+        public BinarySearchTree<T> left;
+
+        /// <summary>
+        /// The right branch tree
+        /// </summary>
+        public BinarySearchTree<T> right;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinarySearchTree{T}"/> class.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public BinarySearchTree(T data)
+        {
+            this.rootData = data;
+            this.left = null;
+            this.right = null;
+        }
+
+        /// <summary>
+        /// Inserts the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public void Insert(T data)
+        {
+            if (data.CompareTo(rootData) <= 0)
+            {
+                if(this.left == null)
+                {
+                    this.left = new BinarySearchTree<T>(data);
+                }
+                else
+                {
+                    this.left.Insert(data);
+                }
+            }
+            else
+            {
+                if(this.right == null)
+                {
+                    this.right = new BinarySearchTree<T>(data);
+                }
+                else
+                {
+                    this.right.Insert(data);
+                }
+            }
+        }
+
+        /// <summary>
+        /// In order traversal of bst.
+        /// </summary>
+        public void InOrderTraversal()
+        {
+            if (this.left != null)
+            {
+                this.left.InOrderTraversal();
+            }
+            Console.Write(this.rootData + " ");
+            if (this.right != null)
+            {
+                this.right.InOrderTraversal();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BinarySearchTree.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akshay Poriya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BST_DS
+{
+    using System;
+
+    /// <summary>
+    /// Contains Main Function
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.InOrderTraversal();
+        }
+    }
+}
